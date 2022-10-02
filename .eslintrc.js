223925c2a38af0cec4a9cbe8a7e511455b3730d6
Denisse23 <denissesuazo@hotmail.com>
2022-10-02 01:05:06 -0600

    Configure prettier formatter instead of standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,53 +2,12 @@ module.exports = {
   root: true,
   extends: [
     '@react-native-community/eslint-config', // Default RN config
-    'standard-with-typescript', // RN plugins for standard with typescritp
-    'eslint-config-prettier' // Disable prettier rules that conflict with eslint
+    'plugin:prettier/recommended',
   ],
-  parser: '@typescript-eslint/parser', // RN plugins for standard with typescritp
+  parser: '@typescript-eslint/parser', // RN plugins for typescritp
   plugins: [
     '@typescript-eslint', // RN plugins for typescritp
     'react', // RN plugins for react
-    'react-native' // RN plugins for react-native
+    'react-native', // RN plugins for react-native
   ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
-    },
-    'project': './tsconfig.json' // Required for Standard plugin
-  },
-  'env': {
-    'react-native/react-native': true
-  },
-  'rules': {
-    'prettier/prettier': 'off', // Turn off prettier
-    'react-native/no-unused-styles': 'warn',
-    'react-native/no-inline-styles': 'error',
-    'react-native/no-raw-text': ['warn', {
-      skip: ['CustomText']
-    }],
-    'react-native/no-single-element-style-arrays': 'warn',
-    'object-curly-spacing': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/require-array-sort-compare': ['error', {
-      ignoreStringArrays: true
-    }],
-    'react/jsx-curly-spacing': ['error', {
-      when: 'always',
-      allowMultiline: true,
-      children: true
-    }],
-    'eol-last': ['error', 'always'],
-    'no-multiple-empty-lines': 'error',
-    semi: ['error', 'never'],
-    // Indent with 2 spaces
-    indent: ['error', 2],
-    // Indent JSX with 2 spaces
-    'react/jsx-indent': ['error', 2],
-    // Indent props with 2 spaces
-    'react/jsx-indent-props': ['error', 2]
-  }
-}
+};
